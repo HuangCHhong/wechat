13369abb53625c5e748d91e6f356708cc79811fa
holden <1099975174@qq.com>
2018-07-22 14:54:01 +0800

    test

---
+++ application/controllers/Index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: holdenhuang
+ * Date: 2018/7/22
+ * Time: 14:31
+ */
+
+class Index extends CI_Controller
+{
+
+    public function get()
+    {
+        $token = 'hchhch520';
+        $signature = $_GET["signature"];
+        $timestamp  = $_GET['timestamp'];
+        $nonce = $_GET['nonce'];
+        $echostr = $_GET['echostr'];
+        $tmpArr = array($timestamp, $nonce,$token);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $signature == $tmpStr){
+            return $echostr;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ application/models/Index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: holdenhuang
+ * Date: 2018/7/22
+ * Time: 14:34
+ */
+
+class Index
+{
+
+}
